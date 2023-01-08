f8c13e053c192aca989a462102f1d44b788e5650
Tomano <vetom@centrum.cz>
2023-01-08 15:21:49 +0100

    Add Black olives Fruit

--- src/main/java/tnt/blockychef/common/Registry.java
+++ src/main/java/tnt/blockychef/common/Registry.java
@@ -223,6 +223,7 @@ public final class Registry {
         helper.register("apple_fruit", new TreeHangingFruitBlock(), false);
         helper.register("avocado_fruit", new TreeHangingFruitBlock(), false);
         helper.register("banana_fruit", new TreeHangingFruitBlock(), false);
+        helper.register("black_olives_fruit", new TreeHangingFruitBlock(), false);
     }
 
     private static void registerItems(RegisterEvent.RegisterHelper<Item> helper) {
@@ -260,6 +261,7 @@ public final class Registry {
         helper.register("apple", new Item(new Item.Properties()));
         helper.register("avocado", new Item(new Item.Properties()));
         helper.register("banana", new Item(new Item.Properties()));
+        helper.register("black_olives", new Item(new Item.Properties()));
     }
 
     @FunctionalInterface // Registers blocks and schedules itemBlock registration
--- src/main/java/util/GenerateFruitModels.java
+++ src/main/java/util/GenerateFruitModels.java
@@ -15,7 +15,7 @@ import java.util.List;
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class GenerateFruitModels {
 
-    private static final String FRUIT_NAME = "banana";
+    private static final String FRUIT_NAME = "black_olives";
     private static final String FRUIT_SUFFIX = "_fruit";
     private static final int LOOT_BONUS = 2;
     private static final ModelVariant MODEL_VARIANT = ModelVariant.MEDIUM;
