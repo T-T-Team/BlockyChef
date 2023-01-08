c28ef2f3482e459e56404ec5f8c22a1c6cf0b938
Tomano <vetom@centrum.cz>
2023-01-08 15:37:17 +0100

    Add Coconut Fruit

--- src/main/java/tnt/blockychef/common/Registry.java
+++ src/main/java/tnt/blockychef/common/Registry.java
@@ -225,6 +225,7 @@ public final class Registry {
         helper.register("banana_fruit", new TreeHangingFruitBlock(), false);
         helper.register("black_olives_fruit", new TreeHangingFruitBlock(), false);
         helper.register("cherry_fruit", new TreeHangingFruitBlock(), false);
+        helper.register("coconut_fruit", new TreeHangingFruitBlock(), false);
     }
 
     private static void registerItems(RegisterEvent.RegisterHelper<Item> helper) {
@@ -264,6 +265,7 @@ public final class Registry {
         helper.register("banana", new Item(new Item.Properties()));
         helper.register("black_olives", new Item(new Item.Properties()));
         helper.register("cherry", new Item(new Item.Properties()));
+        helper.register("coconut", new Item(new Item.Properties()));
     }
 
     @FunctionalInterface // Registers blocks and schedules itemBlock registration
