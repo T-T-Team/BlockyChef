1a029aa62cae0f67b7177f1f23f0c1e67b4f3c0b
Toma <novotny.tom96@gmail.com>
2023-01-28 01:15:01 +0100

    Rename items

--- src/main/java/tnt/blockychef/common/init/BlockyChefItems.java
+++ src/main/java/tnt/blockychef/common/init/BlockyChefItems.java
@@ -11,8 +11,8 @@ public final class BlockyChefItems {
     public static final Item CUCUMBER = null;
     @ObjectHolder(value = "blockychef:bell_pepper", registryName = "item")
     public static final Item BELL_PEPPER = null;
-    @ObjectHolder(value = "blockychef:black_pepper", registryName = "item")
-    public static final Item BLACK_PEPPER = null;
+    @ObjectHolder(value = "blockychef:black_pepper_cluster", registryName = "item")
+    public static final Item BLACK_PEPPER_CLUSTER = null;
     @ObjectHolder(value = "blockychef:blueberry", registryName = "item")
     public static final Item BLUEBERRY = null;
     @ObjectHolder(value = "blockychef:broccoli", registryName = "item")
@@ -21,8 +21,8 @@ public final class BlockyChefItems {
     public static final Item CABBAGE = null;
     @ObjectHolder(value = "blockychef:chilli_pepper", registryName = "item")
     public static final Item CHILLI_PEPPER = null;
-    @ObjectHolder(value = "blockychef:corn", registryName = "item")
-    public static final Item CORN = null;
+    @ObjectHolder(value = "blockychef:corn_cob", registryName = "item")
+    public static final Item CORN_COB = null;
     @ObjectHolder(value = "blockychef:garlic", registryName = "item")
     public static final Item GARLIC = null;
     @ObjectHolder(value = "blockychef:leek", registryName = "item")
@@ -43,30 +43,30 @@ public final class BlockyChefItems {
     public static final Item TURMERIC = null;
     @ObjectHolder(value = "blockychef:pineapple", registryName = "item")
     public static final Item PINEAPPLE = null;
-    @ObjectHolder(value = "blockychef:portobello", registryName = "item")
-    public static final Item PORTOBELLO = null;
-    @ObjectHolder(value = "blockychef:beans", registryName = "item")
-    public static final Item BEANS = null;
-    @ObjectHolder(value = "blockychef:peanuts", registryName = "item")
-    public static final Item PEANUTS = null;
-    @ObjectHolder(value = "blockychef:peas", registryName = "item")
-    public static final Item PEAS = null;
-    @ObjectHolder(value = "blockychef:soybeans", registryName = "item")
-    public static final Item SOYBEANS = null;
+    @ObjectHolder(value = "blockychef:portobello_mushroom", registryName = "item")
+    public static final Item PORTOBELLO_MUSHROOM = null;
+    @ObjectHolder(value = "blockychef:bean_pod", registryName = "item")
+    public static final Item BEAN_POD = null;
+    @ObjectHolder(value = "blockychef:peanut", registryName = "item")
+    public static final Item PEANUT = null;
+    @ObjectHolder(value = "blockychef:pea_pod", registryName = "item")
+    public static final Item PEA_POD = null;
+    @ObjectHolder(value = "blockychef:soybean", registryName = "item")
+    public static final Item SOYBEAN_POD = null;
     @ObjectHolder(value = "blockychef:basil", registryName = "item")
     public static final Item BASIL = null;
-    @ObjectHolder(value = "blockychef:ginger", registryName = "item")
-    public static final Item GINGER = null;
-    @ObjectHolder(value = "blockychef:tea", registryName = "item")
-    public static final Item TEA = null;
+    @ObjectHolder(value = "blockychef:ginger_root", registryName = "item")
+    public static final Item GINGER_ROOT = null;
+    @ObjectHolder(value = "blockychef:tea_leaf", registryName = "item")
+    public static final Item TEA_LEAF = null;
     @ObjectHolder(value = "blockychef:grapes", registryName = "item")
     public static final Item GRAPES = null;
-    @ObjectHolder(value = "blockychef:rice", registryName = "item")
-    public static final Item RICE = null;
-    @ObjectHolder(value = "blockychef:oat", registryName = "item")
-    public static final Item OAT = null;
-    @ObjectHolder(value = "blockychef:mustard", registryName = "item")
-    public static final Item MUSTARD = null;
-    @ObjectHolder(value = "blockychef:coffee", registryName = "item")
-    public static final Item COFFEE = null;
+    @ObjectHolder(value = "blockychef:rice_crop", registryName = "item")
+    public static final Item RICE_CROP = null;
+    @ObjectHolder(value = "blockychef:oat_crop", registryName = "item")
+    public static final Item OAT_CROP = null;
+    @ObjectHolder(value = "blockychef:mustard_pods", registryName = "item")
+    public static final Item MUSTARD_PODS = null;
+    @ObjectHolder(value = "blockychef:coffee_beans_cluster", registryName = "item")
+    public static final Item COFFEE_BEANS_CLUSTER = null;
 }
